921fd96ac93d403e7fd07d52fe17cd980c78fcf3
Amit Kumar Singh <amitkumarsingh2805@gmail.com>
2019-09-30 19:55:18 +0530

    Add files via upload

---
+++ automate2.ps1
@@ -0,0 +1,18 @@
+﻿
+#Login to Azure using powershell
+$password = ConvertTo-SecureString 'password' -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential ('uname', $password)
+Login-AzureRmAccount -Credential $Credential
+
+#Setting up location
+$location="West Europe"  
+
+#Create a resource group.  
+New-AzureRmResourceGroup -Name $(rgname) -Location $location  
+
+#Create an App Service plan in Free tier.  
+New-AzureRmAppServicePlan -Name $(asp) -Location $location -ResourceGroupName $(rgname) -Tier Free  
+
+
+#Create a web app.   
+New-AzureRmWebApp -Name $(app) -Location $location -AppServicePlan $(asp) -ResourceGroupName $(rgname)
